88084c98ee4ae182fc37f6a4bf9c8d7dd52c8b1a
Afraid Khan <bettern211@gmail.com>
2025-11-19 14:17:27 +0600

    config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# Telegram Bot token
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-
-# Spotify API credentials (server-side, only you need them)
-SPOTIFY_CLIENT_ID = "YOUR_OWN_CLIENT_ID"
-SPOTIFY_CLIENT_SECRET = "YOUR_OWN_CLIENT_SECRET"
